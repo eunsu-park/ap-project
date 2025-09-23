8992691a262da94c3282ed2ea9d833d63948f25f
Eunsu Park <phd.choux@gmail.com>
2025-09-22 22:40:52 -0400

    Create realtime.sh

---
+++ new_acquisition/realtime.sh
@@ -0,0 +1,2 @@
+python download_sdo_hmi_jp2.py --parallel 8 --destination-root /Users/eunsupark/ap_data --days 10
+python download_lasco_realtime.py --parallel 8 --destination-root /Users/eunsupark/ap_data --days 10
