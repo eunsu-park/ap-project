f5219f5f63b6b9d84d92d74d13179a2fc007d24d
Eunsu Park <phd.choux@gmail.com>
2025-10-21 16:40:15 -0400

    update

--- new_model/auto_validation.py
+++ new_model/auto_validation.py
@@ -28,60 +28,76 @@ fixed = [
 
 if __name__ == "__main__" :
 
-    base_config_path = "./configs/wulver_G1_full.yaml"
+    base_config_names = [
+        "G1_3_to_1",
+        "G1_3_to_1_1month",
+        "G1_3_to_2",
+        "G1_3_to_3",
 
-    with open(base_config_path, 'r') as f:
-        base_config = yaml.safe_load(f)
+        "G1_4_to_1",
+        "G1_4_to_1_1month",
+        "G1_4_to_2",
+        "G1_4_to_3",
 
-    save_root = base_config["environment"]["save_root"]
-    experiment_name = base_config["experiment"]["experiment_name"]
-    experiment_dir = f"{save_root}/{experiment_name}"
-    checkpoint_dir = f"{experiment_dir}/checkpoint"
-    output_root = f"{experiment_dir}/output"
+        "G1_5_to_1",
+        "G1_5_to_1_1month",
+        "G1_5_to_2",
+        "G1_5_to_3"
+        ]
+    
+    for base_config_name in base_config_names :
 
-    for n in range(10):
+        base_config = yaml.safe_load(f"./configs/{base_config_name}.yaml")
 
-        lines = fixed.copy()
-        epoch = (n+1)*50
+        save_root = base_config["environment"]["save_root"]
+        experiment_name = base_config["experiment"]["experiment_name"]
+        experiment_dir = f"{save_root}/{experiment_name}"
+        checkpoint_dir = f"{experiment_dir}/checkpoint"
+        output_root = f"{experiment_dir}/output"
 
-        job_name = f"class_{epoch:03d}"
-        print(f"Submitting job: {job_name}")
-        lines.insert(2, f"#SBATCH --job-name={job_name}")
+        for n in range(10):
 
-        checkpoint_path = f"{checkpoint_dir}/model_epoch{epoch}.pth"
-        if not os.path.exists(checkpoint_path):
-            print(f"Checkpoint {checkpoint_path} does not exist, skipping...")
-            continue
-        else :
-            print(f"Found checkpoint: {checkpoint_path}")
+            lines = fixed.copy()
+            epoch = (n+1)*50
 
-        output_dir = f"{output_root}/epoch_{epoch:03d}"
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-            print(f"Created output directory: {output_dir}")
+            job_name = f"class_{epoch:03d}"
+            print(f"Submitting job: {job_name}")
+            lines.insert(2, f"#SBATCH --job-name={job_name}")
 
-        config = base_config.copy()
-        config["validation"] = {
-            "checkpoint_path": checkpoint_path,
-            "output_dir": output_dir
-        }
+            checkpoint_path = f"{checkpoint_dir}/model_epoch{epoch}.pth"
+            if not os.path.exists(checkpoint_path):
+                print(f"Checkpoint {checkpoint_path} does not exist, skipping...")
+                continue
+            else :
+                print(f"Found checkpoint: {checkpoint_path}")
 
-        config_name = f"{experiment_name}_epoch{epoch:03d}"
-        config_path = f"./configs/{config_name}.yaml"
-        with open(config_path, 'w') as f:
-            yaml.dump(config, f)
-            print(f"Saved config to: {config_path}")
+            output_dir = f"{output_root}/epoch_{epoch:03d}"
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+                print(f"Created output directory: {output_dir}")
 
-        lines.append( f"/home/hl545/miniconda3/envs/ap/bin/python validation.py --config-name {config_name}")
+            config = base_config.copy()
+            config["validation"] = {
+                "checkpoint_path": checkpoint_path,
+                "output_dir": output_dir
+            }
 
-        with open("tmp.sh", "w") as f:
-            f.write("\n".join(lines))
+            config_name = f"{experiment_name}_epoch{epoch:03d}"
+            config_path = f"./configs/{config_name}.yaml"
+            with open(config_path, 'w') as f:
+                yaml.dump(config, f)
+                print(f"Saved config to: {config_path}")
 
-        command = "sbatch tmp.sh"
-        os.system(command)
+            lines.append( f"/home/hl545/miniconda3/envs/ap/bin/python validation.py --config-name {config_name}")
 
-        time.sleep(5)
-        os.remove("tmp.sh")
-        time.sleep(5)
+            with open("tmp.sh", "w") as f:
+                f.write("\n".join(lines))
+
+            command = "sbatch tmp.sh"
+            os.system(command)
+
+            time.sleep(10)
+            os.remove("tmp.sh")
+            time.sleep(10)
 
         # break
