21860d8bd7059307b11f0f44f2c3a869993315a4
Eunsu Park <phd.choux@gmail.com>
2025-10-30 17:11:09 -0400

    update

--- new_model/check_result_exist.py
+++ new_model/check_result_exist.py
@@ -21,7 +21,24 @@ def main(config):
                 no_dir_list.append(epoch)
         if len(no_dir_list) != 0 :
             print(os.path.basename(result_dir), no_dir_list)
-            
+
+
+    result_list = sorted(glob(f"{save_root}/weighted_days*"))
+    num_result = len(result_list)
+    print(num_result)
+    
+    for result_dir in result_list :
+        output_dir = f"{result_dir}/output"
+        no_dir_list = []
+        for n in range(10):
+            epoch = (n+1) * 100
+            epoch_dir = f"{output_dir}/epoch_{epoch}"
+            if not os.path.exists(epoch_dir):
+                no_dir_list.append(epoch)
+        if len(no_dir_list) != 0 :
+            print(os.path.basename(result_dir), no_dir_list)
+
+
 
 if __name__ == "__main__" :
     main()
---
+++ new_model/clear.sh
@@ -0,0 +1,6 @@
+
+rm ./days*.sh
+rm ./weighted_days*.sh
+
+rm ./configs/days*.yaml
+rm ./configs/weighted_days*.sh
