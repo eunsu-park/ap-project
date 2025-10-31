125d7306beabed410ca79a6016c4fcc40bbac7e9
Eunsu Park <phd.choux@gmail.com>
2025-10-30 22:54:59 -0400

    Create same_number.py

---
+++ new_model/same_number.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from glob import glob
+import os
+
+HOME = os.path.expanduser('~')
+
+
+def calculate_tss(target, output):
+    # Confusion Matrix 요소 계산
+    tp = sum(1 for t, o in zip(target, output) if t == 1 and o == 1)  # True Positive
+    tn = sum(1 for t, o in zip(target, output) if t == 0 and o == 0)  # True Negative
+    fp = sum(1 for t, o in zip(target, output) if t == 0 and o == 1)  # False Positive
+    fn = sum(1 for t, o in zip(target, output) if t == 1 and o == 0)  # False Negative
+    
+    # TPR (True Positive Rate, Sensitivity)
+    tpr = tp / (tp + fn) if (tp + fn) > 0 else 0
+    
+    # FPR (False Positive Rate)
+    fpr = fp / (fp + tn) if (fp + tn) > 0 else 0
+    
+    # TSS = TPR - FPR
+    tss = tpr - fpr
+    
+    return tss
+
+
+def read_csv(file_path):
+      df = pd.read_csv(file_path)
+      target = df["target"].tolist()
+      output = df["output"].tolist()
+      return target, output
+
+
+input_days = range(1, 8)
+output_day = 1
+epochs = range(100, 1001, 100)
+weighted = (False, True)
+subsets = range(10)
+
+result_root = f"{HOME}/ap/results"
+file_name = "validation_results.csv"
+
+for apply_weighted in weighted :
+    for input_day in input_days :
+        for epoch in epochs :
+            for subset in subsets :
+                if apply_weighted is True :
+                    experiment_name = f"weighted_days{input_day}_to_day{output_day}_sub_{subset}"
+                else :
+                    experiment_name = f"days{input_day}_to_day{output_day}_sub_{subset}"
+
+                dir_path = f"{result_root}/{experiment_name}/epoch_{epoch}"
+                file_path = f"{dir_path}/{file_name}"
+
+                print(file_path, os.path.exists(file_path))
+                
+
+
+
