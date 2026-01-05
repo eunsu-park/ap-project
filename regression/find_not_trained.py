14b60967447b32d1d764106751fd5220824711f0
Eunsu Park <phd.choux@gmail.com>
2026-01-04 20:16:50 -0500

    Update find_not_trained.py

--- regression/find_not_trained.py
+++ regression/find_not_trained.py
@@ -4,13 +4,30 @@ import os
 
 RESULT_ROOT = "/home/hl545/ap/renew/results"
 
-PREFIX = "SINGLE"
+def find_single():
+    PREFIX = "SINGLE"
+    for I in range(7):
+        for T in range(3):
+            for S in range(14):
+                dir_path = f"{RESULT_ROOT}/{PREFIX}_{I+1}_{T+1}_{S:02d}"
+                check_path = f"{dir_path}/checkpoint/model_final.pth"
+                if not os.path.exists(check_path):
+                    print(f"{dir_path}: not trained")
 
-for I in range(7):
-    for T in range(3):
-        for S in range(14):
-            dir_path = f"{RESULT_ROOT}/{PREFIX}_{I+1}_{T+1}_{S:02d}"
+def find_multi_12():
+    PREFIX = "MULTITARGET"
+    for I in range(7):
+        for S in range(8):
+            dir_path = f"{RESULT_ROOT}/{PREFIX}_{I+1}_1-2_{S:02d}"
             check_path = f"{dir_path}/checkpoint/model_final.pth"
+            if not os.path.exists(check_path):
+                print(f"{dir_path}: not trained")
 
+def find_multi_13():
+    PREFIX = "MULTITARGET"
+    for I in range(7):
+        for S in range(6):
+            dir_path = f"{RESULT_ROOT}/{PREFIX}_{I+1}_1-3_{S:02d}"
+            check_path = f"{dir_path}/checkpoint/model_final.pth"
             if not os.path.exists(check_path):
                 print(f"{dir_path}: not trained")
