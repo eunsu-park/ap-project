0b61ccdcb8d5178997ebc44d98c41e95632426b4
Eunsu Park <phd.choux@gmail.com>
2026-01-04 21:01:41 -0500

    Update find_not_trained.py

--- regression/find_not_trained.py
+++ regression/find_not_trained.py
@@ -5,7 +5,7 @@ import os
 RESULT_ROOT = "/home/hl545/ap/renew/results"
 
 def find_single():
-    result = []
+    task_list = []
     PREFIX = "SINGLE"
     for I in range(7):
         for T in range(3):
@@ -14,11 +14,15 @@ def find_single():
                 check_path = f"{dir_path}/checkpoint/model_final.pth"
                 if not os.path.exists(check_path):
                     print(f"{dir_path}: not trained")
-                    result.append(dir_path)
-    return result
+                    task_list.append((I, T, S))
+    f = open("not_trained_single.txt", "w")
+    for task in task_list:
+        f.write("\t".join([str(x) for x in task]) + "\n")
+    f.close()
+    return task_list
 
-def find_multi_12():
-    result = []
+def find_multi12():
+    task_list = []
     PREFIX = "MULTITARGET"
     for I in range(7):
         for S in range(8):
@@ -26,11 +30,15 @@ def find_multi_12():
             check_path = f"{dir_path}/checkpoint/model_final.pth"
             if not os.path.exists(check_path):
                 print(f"{dir_path}: not trained")
-                result.append(dir_path)
-    return result
+                task_list.append((I, S))
+    f = open("not_trained_multi12.txt", "w")
+    for task in task_list:
+        f.write("\t".join([str(x) for x in task]) + "\n")
+    f.close()
+    return task_list
 
-def find_multi_13():
-    result = []
+def find_multi13():
+    task_list = []
     PREFIX = "MULTITARGET"
     for I in range(7):
         for S in range(6):
@@ -38,12 +46,18 @@ def find_multi_13():
             check_path = f"{dir_path}/checkpoint/model_final.pth"
             if not os.path.exists(check_path):
                 print(f"{dir_path}: not trained")
-                result.append(dir_path)
-    return result
+                # task_list.append(dir_path)
+                task_list.append((I, S))
+    f = open("not_trained_multi13.txt", "w")
+    for task in task_list:
+        f.write("\t".join([str(x) for x in task]) + "\n")
+    f.close()
+
+    return task_list
 
 if __name__ == "__main__":
-    result = []
-    result += find_single()
-    result += find_multi_12()
-    result += find_multi_13()
-    print(len(result))
+    task_list = []
+    task_list += find_single()
+    task_list += find_multi12()
+    task_list += find_multi13()
+    print(len(task_list))
